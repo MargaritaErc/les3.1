39e3fd5a4c2cf6bb23c626115298077a7c7e3fb8
m.rita1650@gmail.com <m.rita1650@gmail.com>
2022-11-29 17:26:58 +0300

    zadanie

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vvedite N");
+int N =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int count =1;
+while(count<=N)
+{Console.WriteLine(Math.Pow(count,3));
+count++;
+}
